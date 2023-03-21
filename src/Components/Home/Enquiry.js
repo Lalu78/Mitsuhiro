ca0d33fd147dea9016bd98dc9c843b0a1181f70a
lalumandal <lalu.mandal@napses.com>
2023-03-21 18:57:13 +0530

    alignment center

--- src/Components/Home/Enquiry.js
+++ src/Components/Home/Enquiry.js
@@ -38,58 +38,60 @@ function Enquiry() {
 
 
     return (
-        <div className={`h-auto min-h-[570px] px-5 md:px-12 lg:px-[90px] flex-col w-full  bg-no-repeat bg-cover bg-center  `} style={{ backgroundImage: 'url("/images/ENQUIRY.svg")', backgroundRepeat: 'no-repeat' }}>
-            <h1 className='pt-8 font-semibold md:font-bold text-2xl md:text-4xl'>  Enquire Now </h1>
-            <div className=' mt-6 md:ml-40  w-full max-w-[700px]'>
-                <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-6'>
-                    <TextField
-                        type="text"
-                        placeholder="Enter your name"
-                        label='Full Name *'
-                        name="fullName"
-                        id="fullName"
-                        register={register}
-                        errors={errors}
-
-                    />
-                    <div >
+        <div className={`h-auto min-h-[570px] px-5  flex  w-full justify-center border  bg-no-repeat bg-cover bg-center  `} style={{ backgroundImage: 'url("/images/ENQUIRY.svg")', backgroundRepeat: 'no-repeat' }}>
+            <div className='w-full max-w-[800px]'>
+                <h1 className='pt-8 font-semibold md:font-bold text-2xl md:text-4xl text-center'>  Enquire Now </h1>
+                <div className=' mt-6 w-full '>
+                    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-6'>
                         <TextField
                             type="text"
-                            placeholder="Enter your mobile number"
-                            label='Phone Number *'
-                            countryCode={true}
-                            name="phoneNumber"
-                            id="phoneNumber"
+                            placeholder="Enter your name"
+                            label='Full Name *'
+                            name="fullName"
+                            id="fullName"
                             register={register}
                             errors={errors}
 
                         />
-                       
-                    </div>
-                    <TextField
-                        type="email"
-                        placeholder="Enter your mail Id"
-                        label='E-mail ID *'
-                        name="email"
-                        id="email"
-                        register={register}
-                        errors={errors}
+                        <div >
+                            <TextField
+                                type="text"
+                                placeholder="Enter your mobile number"
+                                label='Phone Number *'
+                                countryCode={true}
+                                name="phoneNumber"
+                                id="phoneNumber"
+                                register={register}
+                                errors={errors}
 
-                    />
-                    <TextArea
-                        type="text"
-                        placeholder="For what this enquiry is about"
-                        label='Subject'
-                        name="subject"
-                        id="subject"
-                        register={register}
-                        errors={errors}
+                            />
 
-                    />
-                    <div className='flex justify-center w-full  pb-10'>
-                        <input type='submit' value={'Submit'} className='bg-black text-white font-bold text-base rounded-[6px] h-12 w-40 cursor-pointer ' />
-                    </div>
-                </form>
+                        </div>
+                        <TextField
+                            type="email"
+                            placeholder="Enter your mail Id"
+                            label='E-mail ID *'
+                            name="email"
+                            id="email"
+                            register={register}
+                            errors={errors}
+
+                        />
+                        <TextArea
+                            type="text"
+                            placeholder="For what this enquiry is about"
+                            label='Subject'
+                            name="subject"
+                            id="subject"
+                            register={register}
+                            errors={errors}
+
+                        />
+                        <div className='flex justify-center w-full  pb-10'>
+                            <input type='submit' value={'Submit'} className='bg-black text-white font-bold text-base rounded-[6px] h-12 w-40 cursor-pointer ' />
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     )
