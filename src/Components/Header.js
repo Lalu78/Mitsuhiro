216bbf82ed2534ef9c619fa01e62115e856d7fe2
lalumandal <lalu.mandal@napses.com>
2023-03-30 18:46:13 +0530

    useNavigate added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,14 @@
 import React, { useState, useRef } from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 
 function Header({ className }) {
   const [showMenu, setShowMenu] = useState(false)
+
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate("/mission");
+  }
+
 
   return (
     <div className={`${className} h-20 md:h-[109px] bg-[#303131] w-full px-6 md:px-12 lg:px-[110px] flex justify-between items-center`}>
@@ -10,18 +16,18 @@ function Header({ className }) {
         <Link to='/' ><img src='/images/logoLarge.svg' className='h-20 w-40 md:h-[90px] md:w-[321px] ' alt='logo' /></Link>
       </div>
       <div className='hidden md:flex gap-x-4 md:gap-x-7 lg:gap-x-14 text-[#EBEBEB] font-medium text-sm'>
-        <Link to='about'>AboutUs</Link>
-        <Link to='products'>Products</Link>
-        <Link to='mission'>Mission</Link>
+        <Link to='/about'>AboutUs</Link>
+        <Link to='/products'>Products</Link>
+        <button onClick={handleClick} >Mission</button>
 
       </div>
       <img onClick={() => setShowMenu(!showMenu)} src='/images/menu.svg' alt='menu' className='flex md:hidden' />
       {
         showMenu &&
         <div className='bg-[#6c6e6e] text-white h-40 gap-4 p-4 w-full flex flex-col md:block absolute top-20 left-0'>
-          <Link to='about' onClick={() => setShowMenu(false)}>AboutUs</Link>
-          <Link to='products' onClick={() => setShowMenu(false)}>Products</Link>
-          <Link to='mission' onClick={() => setShowMenu(false)}>Mission</Link>
+          <Link to='/about' onClick={() => setShowMenu(false)}>AboutUs</Link>
+          <Link to='/products' onClick={() => setShowMenu(false)}>Products</Link>
+          <Link to='/mission' onClick={() => setShowMenu(false)}>Mission</Link>
         </div>
       }
 
